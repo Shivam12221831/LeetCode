d3c7bdb7867366aa2eca6920ed8a121db969a715
Shivam12221831 <shiva954882@gmail.com>
2025-03-05 22:22:42 +0530

    Time: 0 ms (100%), Space: 10 MB (97.19%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1=0, prev2=0;
+        for(int i=0; i<nums.size(); i++){
+            int temp = prev1;
+            prev1 = max(prev1, prev2+nums[i]);
+            prev2 = temp;
+        }
+        return prev1;
+    }
+};
