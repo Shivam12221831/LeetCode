d0869de69fcea3cea377d8bb2bd559053ae145cc
Shivam12221831 <shiva954882@gmail.com>
2025-03-23 18:50:24 +0530

    Time: 8 ms (0.91%), Space: 46.6 MB (9.63%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binarySearch(vector<int> nums, int target, int st, int end){
+        if(st <= end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target){
+                return binarySearch(nums, target, mid+1, end);
+            } else {
+                return binarySearch(nums, target, st, mid-1);
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size()-1);
+    }
+};
