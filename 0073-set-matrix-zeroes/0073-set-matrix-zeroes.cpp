3d331a116c027eff45c11633907f3bbd75d832d0
Shivam_Singh <shiva954882@gmail.com>
2025-05-01 00:17:26 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,27 +3,20 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        unordered_map<int, int> mpRow;
-        unordered_map<int, int> mpCol;
-        // vector<vector<int>> ans(n, vector<int>(m, 0));
-
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+        
         for(int i=0; i<n; i++){
-            // int rowZ = 0;
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    if(mpRow.find(i) == mpRow.end()){
-                        mpRow[i] = 1;
-                    }
-                    mpCol[j] = 1;;
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(mpRow.find(i) != mpRow.end()){
-                    matrix[i][j] = 0;
-                }
-                if(mpCol.find(j) != mpCol.end()){
+                if(row[i] || col[j]){
                     matrix[i][j] = 0;
                 }
             }
