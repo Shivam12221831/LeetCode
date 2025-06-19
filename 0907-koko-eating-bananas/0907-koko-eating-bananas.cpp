b21cc4d7ab4a30d94e2abae48adbbe8a258ca4fc
Shivam_Singh <shiva954882@gmail.com>
2025-06-19 09:28:08 +0530

    Time: 2 ms (96.49%), Space: 22.7 MB (96.55%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool isPossible(vector<int> &piles, int n, int h, int mid){
+    bool isPossible(vector<int>& piles, int n, int h, long long mid){
         long long hrs = 0;
-        for(int pile : piles){
-            hrs += ((pile-1)/mid)+1;
+        for(int val : piles){
+            hrs += ((val-1)/mid)+1;
         }
-        return hrs <= h ? true : false;
+        return hrs <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        if(h < n) return -1;
+        if(h < n ) return -1;
         long long s = 1, e = *max_element(piles.begin(), piles.end());
         int ans = -1;
         while(s <= e){
