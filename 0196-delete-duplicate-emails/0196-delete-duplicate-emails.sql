3bb339faedd54e863efb34006ac7eddc3d8b2528
Shivam_Singh <shiva954882@gmail.com>
2025-07-07 10:44:14 +0530

    Time: 537 ms (92.69%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 
-with CTE as (select first_value(id) over(partition by email order by id) as unique_email_ids from person)
-delete from person
-where id not in (select * from CTE);
+-- with CTE as (select first_value(id) over(partition by email order by id) as unique_email_ids from person)
+-- delete from person
+-- where id not in (select * from CTE);
+
+
+with CTE as ( select id, row_number() over(partition by email order by id) as rn from person )
+delete from person where id in (select id from CTE where rn > 1);
