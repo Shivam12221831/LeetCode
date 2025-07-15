2b6f9623656c70efd7dd3bfda0981bb5c52a619f
Shivam_Singh <shiva954882@gmail.com>
2025-07-16 00:53:46 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,14 +4,17 @@ public:
         int idx = nums1.size()-1;
         int i = m-1;
         int j = n-1;
-        while(i>=0 && j>=0){
-            if(nums2[j]<nums1[i]){
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
                 nums1[idx--] = nums1[i--];
             } else {
                 nums1[idx--] = nums2[j--];
             }
         }
-        while(j>=0){
+        while(i >= 0){
+            nums1[idx--] = nums1[i--];
+        }
+        while(j >= 0){
             nums1[idx--] = nums2[j--];
         }
     }
