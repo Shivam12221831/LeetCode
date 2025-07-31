c1b4dfb9717fafb7934ab6588ee18040072c5157
Shivam_Singh <shiva954882@gmail.com>
2025-07-31 15:53:15 +0530

    Time: 6 ms (32.47%), Space: 11.6 MB (72.72%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,19 +12,22 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-        if(head == NULL) { return head; }
+        if(head == NULL) return head;
         Node* curr = head;
         while(curr != NULL){
             if(curr->child != NULL){
+                // flatten child list
                 Node* next = curr->next;
                 curr->next = flatten(curr->child);
                 curr->next->prev = curr;
                 curr->child = NULL;
 
+                // find tail node
                 while(curr->next != NULL){
                     curr = curr->next;
                 }
 
+                // connect tail to next
                 if(next != NULL){
                     curr->next = next;
                     next->prev = curr;
